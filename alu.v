861ee2b978019a3badf9f7bdea489290f4993133
Diego Zepeda <38699812+diezep@users.noreply.github.com>
2020-12-01 18:32:59 -0600

    Add files via upload

---
+++ alu.v
@@ -0,0 +1,43 @@
+module ALU(
+	input[31:0] OP1,
+	input[31:0] OP2,
+	input[2:0] OP,
+	output reg[31:0] OPS,
+	output reg ZF
+);
+
+always @(*)
+begin
+
+	case(OP)
+		3'b000:
+			assign OPS = OP1 & OP2;
+		
+		3'b001:
+			assign OPS = OP1 | OP2;
+		
+		3'b010:
+			assign OPS = OP1 + OP2;
+		
+		3'b011:
+			assign OPS = OP1 - OP2;	
+		
+		3'b100:
+			assign OPS = OP1 < OP2 ? 1 : 0;
+			
+		3'b101:
+			assign OPS = OP1 / OP2;
+		
+		3'b110:
+			assign OPS = 32'd0;
+		
+		3'b111:
+			assign OPS = OP1 * OP2;
+	endcase
+
+	if(OPS)
+		ZF <= 1;
+	else 
+		ZF <= 0;
+end
+endmodule
