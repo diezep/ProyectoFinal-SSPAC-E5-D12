514651cc6b8b41cd4678947c788c184a41c480de
Diego Zepeda <38699812+diezep@users.noreply.github.com>
2020-12-03 22:50:21 -0600

    Definido RegDist para inst tipo R.

--- UC.v
+++ UC.v
@@ -1,14 +1,14 @@
 
 module UC(
-input [5:0]inscod,
-output reg RegDist,
-output reg Branch,
-output reg MemRead,
-output reg Memtoreg,
-output reg [2:0]ALUop,
-output reg MemWrite,
-output reg ALUsrc,
-output reg Regwrite
+	input [5:0]inscod,
+	output reg RegDist,
+	output reg Branch,
+	output reg MemRead,
+	output reg Memtoreg,
+	output reg [2:0]ALUop,
+	output reg MemWrite,
+	output reg ALUsrc,
+	output reg Regwrite
 );
 //Definimos los reg y wires
 
@@ -16,15 +16,16 @@ always@*
 begin
 	case(inscod) 
 	 6'b000000: 
-	 begin
-	  Regwrite=1;
-	  MemWrite=0;
-	  Memtoreg=0;
-	  ALUsrc=0;
-	  Branch=0;
-	  MemRead=0;
-	  ALUop=3'b111;
-	 end
-endcase
+		begin
+			RegDist=1;
+			Regwrite=1;
+			MemWrite=0;
+			Memtoreg=0;
+			ALUsrc=0;
+			Branch=0;
+			MemRead=0;
+			ALUop=3'b111;
+		end
+	endcase
 end
 endmodule
