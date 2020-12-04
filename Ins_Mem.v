29a12a0cfe25d84161b38744aa633a35d6a469de
OscarRamos120 <75262728+OscarRamos120@users.noreply.github.com>
2020-12-03 19:39:31 -0600

    Add files via upload

---
+++ Ins_Mem.v
@@ -0,0 +1,9 @@
+
+module Ins_Mem(
+input pcin,
+output reg dataout
+);
+//Reg y wire
+reg [31:0]insMem[31:0];
+
+endmodule 
---
+++ UC.v
@@ -0,0 +1,30 @@
+
+module UC(
+input [5:0]inscod,
+output reg RegDist,
+output reg Branch,
+output reg MemRead,
+output reg Memtoreg,
+output reg [2:0]ALUop,
+output reg MemWrite,
+output reg ALUsrc,
+output reg Regwrite
+);
+//Definimos los reg y wires
+
+always@*
+begin
+	case(inscod) 
+	 6'b000000: 
+	 begin
+	  Regwrite=1;
+	  MemWrite=0;
+	  Memtoreg=0;
+	  ALUsrc=0;
+	  Branch=0;
+	  MemRead=0;
+	  ALUop=3'b111;
+	 end
+endcase
+end
+endmodule
