c9920663e64739a8899fa2f49025594d825f83ff
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2020-12-04 23:53:05 -0600

    Update aluc.v

--- aluc.v
+++ aluc.v
@@ -5,38 +5,38 @@ module ALUC (
 );
 
 
-always @(*) begin
-    case (UC_aluOp)
-        3'b111: 
-            begin
-                case (func)
-                    // AND
-                    6'b100100: 
-                        ALU_aluOp <= 3'b000;
-                    // OR
-                    6'b100101: 
-                        ALU_aluOp <= 3'b001;
-                    // ADD
-                    6'b100000: 
-                        ALU_aluOp <= 3'b010;
-                    // SUB
-                    6'b100010: 
-                        ALU_aluOp <= 3'b011;
-                    // SLT
-                    6'b101010: 
-                        ALU_aluOp <= 3'b100;
-                    // DIV
-                    6'b011010: 
-                        ALU_aluOp <= 3'b101;
-                    // NOP
-                    6'b000000: 
-                        ALU_aluOp <= 3'b110;
-                    // MULT
-                    6'b011000: 
-                        ALU_aluOp <= 3'b111;
-                endcase
-            end
-    endcase
-end
-
+always @*
+    begin
+        case(UC_aluOp)
+            3'b111: 
+                begin
+                    case(func)
+                        // AND
+                        6'b100100: 
+                            ALU_aluOp <= 3'b000;
+                        // OR
+                        6'b100101: 
+                            ALU_aluOp <= 3'b001;
+                        // ADD
+                        6'b100000: 
+                            ALU_aluOp <= 3'b010;
+                        // SUB
+                        6'b100010: 
+                            ALU_aluOp <= 3'b011;
+                        // SLT
+                        6'b101010: 
+                            ALU_aluOp <= 3'b100;
+                        // DIV
+                        6'b011010: 
+                            ALU_aluOp <= 3'b101;
+                        // NOP
+                        6'b000000: 
+                            ALU_aluOp <= 3'b110;
+                        // MULT
+                        6'b011000: 
+                            ALU_aluOp <= 3'b111;
+                    endcase
+                end
+        endcase
+    end
 endmodule
