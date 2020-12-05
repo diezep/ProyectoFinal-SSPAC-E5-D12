6f20699fb5b8d75a49127cd7c0843dea9182c1dc
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2020-12-04 23:55:11 -0600

    No es necesaria otra entrada

--- add.v
+++ add.v
@@ -1,11 +1,10 @@
 module ADD(
-    input [31:0] OP,
-    input [31:0] ADDER,
-    output reg[31:0] OPS    
+    input [31:0]OP,
+    output reg[31:0]OPS    
 );
 
-always @(*) begin
-    OPS = OP + ADDER;
+always @* begin
+    OPS = OP+8'd4;
 end
 
 endmodule
