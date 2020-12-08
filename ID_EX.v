548dee0a1ae1b7decc8519a2bbdf5e46f2c32172
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2020-12-08 13:34:24 -0600

    Update ID_EX.v

--- ID_EX.v
+++ ID_EX.v
@@ -5,47 +5,50 @@ module ID_EX (
     input iMemWrite,
     input iMemRead,
     input iBranch,
-    input [2:0] iAluOP,
+    input [3:0] iAluOP,
     input iALUSrc,
     input iRegDst,
+    input ijump,
     input [31:0] iAddPC,
     input [31:0] iData1,
     input [31:0] iData2,
     input [31:0] iSignExtend,
-    input [31:0] iInstr2016,
-    input [31:0] iInstr1511,
+    input [4:0] iInstr2016,
+    input [4:0] iInstr1511,
     output reg oRegWrite,
     output reg oMemToReg,
     output reg oMemWrite,
     output reg oMemRead,
     output reg oMemBranch,
-    output reg [2:0] oAluOP,
+    output reg [3:0] oAluOP,
     output reg oALUSrc,
     output reg oRegDst,
+    output reg ojump,
     output reg [31:0] oAddPC,
     output reg [31:0] oData1,
     output reg [31:0] oData2,
     output reg [31:0] oSignExtend,
-    output reg [31:0] oInstr2016,
-    output reg [31:0] oInstr1511
+    output reg [4:0] oInstr2016,
+    output reg [4:0] oInstr1511
 );
 
-always @(posedge clk) begin
-    oRegWrite       <=iRegWrite;
-    oMemToReg       <=iMemToReg;
-    oMemWrite       <=iMemWrite;
-    oMemRead        <=iMemRead;
-    oMemBranch      <=iMemBranch;
-    oAluOP          <=iAluOP;
-    oALUSrc         <=iALUSrc;
-    oRegDst         <=iRegDst;
-    oAddPC          <=iAddPC;
-    oData1          <=iData1;
-    oData2          <=iData2;
-    oSignExtend     <=iSignExtend;
-    oInstr2016      <=iInstr2016;
-    oInstr1511      <=iInstr1511;
-    
+always @(posedge clk) 
+begin
+    oRegWrite <= iRegWrite;
+    oMemToReg <= iMemToReg;
+    oMemWrite <= iMemWrite;
+    oMemRead  <= iMemRead;
+    oMemBranch <= iBranch;
+    oAluOP <= iAluOP;
+    oALUSrc <= iALUSrc;
+    ojump <= ijump;
+    oRegDst <= iRegDst;
+    oAddPC <= iAddPC;
+    oData1 <= iData1;
+    oData2 <= iData2;
+    oSignExtend <= iSignExtend;
+    oInstr2016 <= iInstr2016;
+    oInstr1511 <= iInstr1511;
 end
 
 endmodule
